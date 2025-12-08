978d47c94d1a3b8290508da96aebc59ec66b67ce
sikenali <mrchiang1015@outlook.com>
2025-12-08 10:36:25 +0800

    update blog more

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -382,10 +382,10 @@ export default function BlogPage() {
 							animate={{ opacity: 1, scale: 1 }}
 							whileHover={{ scale: 1.05 }}
 							whileTap={{ scale: 0.95 }}
-							href='https://juejin.cn/user/2427311675422382/posts'
+							href='https://www.10012049.xyz'
 							target='_blank'
 							className='card text-secondary static inline-flex items-center gap-2 rounded-xl px-4 py-2 text-xs'>
-							<JuejinSVG className='h-4 w-4' />
+							<WechatSVG className='h-4 w-4' />
 							更多
 						</motion.a>
 					</div>
